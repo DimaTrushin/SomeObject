abe0bebd01e9b4b296c4b0665c397a1618678971
Dima Truhsin <trushindima@yandex.ru>
2020-01-30 11:47:41 +0300

    added how to and a specialization for the filter function

--- SomeObject.h
+++ SomeObject.h
@@ -4,6 +4,81 @@
 #include <variant>
 #include <functional>
 
+//---------------------------------------------------------------------------
+// How To Use
+//---------------------------------------------------------------------------
+//
+// Description
+// CSomeObject allows you to store an object of a class from a list of
+// different classes. It does not allocate any memory on the heap.
+// The basic template CSomeObject only stores data without any access
+// to the data. You can extend the interface of the class by adding
+// new methods to its interface and even control the behaviour depending
+// on the type currently stored in the class. CSomeObject can be in an empty
+// state.
+//
+// CSomeObject has value semantics. Since it does not allocate, its move
+// operations are as bad as the worst one among the types provided.
+//
+// Currently CSomeObject uses std::variant inside. Hence it has all
+// its restrictions on the stored classses.
+//
+// 1) Publicly inherit from CSomeObject. Open all its constructors and
+// provide public methods in the interface.
+//
+// template<class... TObjects>
+// class CSomePrint : public CSomeObject<TObjects...> {
+//   using CBase = CSomeObject<TObjects...>;
+// public:
+//   using CBase::CBase;
+//   void print() const {
+//     CBase::call([](auto&& arg) {
+//       if constexpr(CBase::isOneOf<decltype(arg), int, char>())
+//         std::cout << "print int or char() = " << arg << std::endl;
+//       else
+//         std::cout << "print other() = " << arg << std::endl;
+//     },[](auto&&) {
+//       std::cout << "empty" << std::endl;
+//     });
+//   }
+// };
+//
+// 2) The pattern to implement a method is this
+//
+//   void method_name const {
+//     CBase::call([](auto&& arg) {
+//       // implementation for each non empty case
+//     },[](auto&&) {
+//       // implementation for the empty case
+//     });
+//   }
+//
+// If you want doing nothing in the empty case, then just use this version
+//
+//   void method_name const {
+//     CBase::call([](auto&& arg) {
+//       // implementation for non empty case
+//     });
+//   }
+//
+// You could implement the print method simply by
+//
+//   void print() const {
+//     CBase::call([](auto&& arg) {
+//       std::cout << "print() = " << arg << std::endl;
+//     });
+//   }
+//
+// In case you need different behaviour for different types you are storing
+// use if constexpr, e.g.,
+//
+// if constexpr (CBase::isOneOf<decltype(arg), int>())
+//
+// is used to check if the current type is int or you may use several types
+//
+// if constexpr(CBase::isOneOf<decltype(arg), int, char>())
+//
+//---------------------------------------------------------------------------
 
 template<class... TObjects>
 class CSomeObject {
@@ -45,6 +120,20 @@ protected:
     return (std::is_same_v<T_, T1> ? true : isOneOf<T, Ts...>());
   }
 
+  template<class TArg, class TFunction>
+  static auto filter(TArg&& arg, TFunction&& function) {
+    using CArg_ = std::decay_t<TArg>;
+    if constexpr(!std::is_same_v<CArg_, CEmpty>) {
+      return std::invoke(std::forward<TFunction>(function),
+                         std::forward<TArg>(arg));
+    } else {
+      // Need this to suppress the unused warnings in case
+      // there is no code in the function if the type of the argument is CEmpty
+      (void)function;
+      (void)arg;
+    }
+  }
+
   template<class TArg, class TFunction1, class TFunction2>
   static auto filter(TArg&& arg, TFunction1&& function1, TFunction2&& function2) {
     using CArg_ = std::decay_t<TArg>;
@@ -58,7 +147,6 @@ protected:
       // Need this to suppress the unused warnings in case
       // there is no code in the function if the type of the argument is CEmpty
       (void)function1;
-      //(void)arg;
     }
   }
 
@@ -68,8 +156,7 @@ protected:
   void call(TFunction&& function) {
     std::visit([&function, this](auto&& arg) {
       return filter(std::forward<decltype(arg)>(arg), // should be the same as static_cast<decltype(arg)&&>
-                    std::forward<TFunction>(function),
-      [](auto&&) {});
+                    std::forward<TFunction>(function));
     }, Object_);
   }
 
@@ -77,8 +164,7 @@ protected:
   void call(TFunction1&& function1) const {
     std::visit([&function1, this](auto&& arg) {
       return filter(std::forward<decltype(arg)>(arg), // should be the same as static_cast<decltype(arg)&&>
-                    std::forward<TFunction1>(function1),
-      [](auto&&) {});
+                    std::forward<TFunction1>(function1));
     }, Object_);
   }
 
